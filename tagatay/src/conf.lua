91777563eebd4051a4375cc72bd14139b310ba16
EmilyClaudet <emily@kano.me>
2016-05-03 00:18:16 +0100

    trying new map plus added conf

---
+++ tagatay/src/conf.lua
@@ -0,0 +1,17 @@
+function love.conf(t)
+	t.modules.joystick = true
+	t.modules.audio = true
+	t.modules.keyboard = true
+	t.modules.event = true
+	t.modules.image = true
+	t.modules.graphics = true
+	t.modules.timer = true
+	t.modules.mouse = true
+	t.modules.sound = true
+	t.title = "Tagaytay Game"
+	t.author = "www.emilyclaudet.com"
+	t.window.vsync = false
+	t.window.fsaa = 0
+	t.window.height = 768
+	t.window.width = 1024
+end
